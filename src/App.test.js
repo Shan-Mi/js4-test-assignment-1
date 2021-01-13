106ca1eaab800ff1e83045449a14a1bbb5ac8c15
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-01-13 22:03:11 +0100

    test context

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { mount } from "enzyme";
+import App from "./App";
+import UserContextProvider from "./contexts/UserContextProvider";
+import { BrowserRouter } from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Test context", () => {
+  it(`Renders 'me' info correctly in internal-link`, () => {
+    const me = {
+      event: "summerburst",
+      firstName: "John",
+      lastName: "Doe",
+      username: "johndoe",
+      email: "john@doe.com",
+    };
+    const wrapper = mount(
+      <BrowserRouter>
+        <UserContextProvider>
+          <App />
+        </UserContextProvider>
+      </BrowserRouter>
+    );
+    const internalUserinfo = wrapper.find(".internal-link").text();
+    expect(internalUserinfo).toContain(me.username);
+  });
 });
